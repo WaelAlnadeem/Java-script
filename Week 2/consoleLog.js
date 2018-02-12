084ada4c59d038494a411c7777bb5538cf32354e
WaelAlnadeem <35236733+WaelAlnadeem@users.noreply.github.com>
2018-02-12 14:47:32 +0100

    Create consoleLog.js

---
+++ Week 2/consoleLog.js
@@ -0,0 +1,15 @@
+var x = "Hei Verden// Norwegian";
+var y = "hello world // english";
+var z = "Hallo Wereld // dutch";
+var w = "Hallo Welt // german";
+> console.log(x);
+ Hei Verden// Norwegian
+ > console.log(y);
+ hello world // english
+ > console.log(z);
+ Hallo Wereld // dutch
+ > console.log(w);
+ Hallo Welt // german
+ 
+ 
+ 
