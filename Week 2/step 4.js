998fb024dad478df56c57bd8cce00c49939b418b
WaelAlnadeem <35236733+WaelAlnadeem@users.noreply.github.com>
2018-02-12 18:29:46 +0100

    Create step 4.js

---
+++ Week 2/step 4.js
@@ -0,0 +1,14 @@
+> var y = "hello world";
+undefined
+console.log('the value of my string will be: hello world');
+undefined
+> console.log (y);
+hello world
+undefined
+> var y ="hello";
+undefined
+console.log('the value of my string will be: hello');
+undefined
+> console.log (y);
+hello
+undefined
