ee4d6070172f54c2fa43e6b9aa72d3ff59b29805
WaelAlnadeem <35236733+WaelAlnadeem@users.noreply.github.com>
2018-02-12 22:54:20 +0100

    Create step 6.js

---
+++ Week 2/step 6.js
@@ -0,0 +1,23 @@
+var arr = []
+undefined
+console.log('the value of my array will be: empty array');
+the value of my array will be: empty array
+undefined
+console.log (arr)
+[]
+undefined
+var animals = ["cat" , "dog" , "wolf"];
+undefined
+console.log (animals);
+["cat", "dog", "wolf"]
+undefined
+var daanFav = "baby pig"; 
+undefined
+console.log (daanFav);
+baby pig
+undefined
+var arr = animals. concat(daanFav); 
+undefined
+console.log (arr);
+["cat", "dog", "wolf", "baby pig"]
+undefined
