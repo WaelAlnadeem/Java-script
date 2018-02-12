d27a0c0c9580e561c5b81afcbc0f3b43dba3fcbf
WaelAlnadeem <35236733+WaelAlnadeem@users.noreply.github.com>
2018-02-12 18:18:49 +0100

    Create var x ; undefined.js

---
+++ Week 2/var x ; undefined.js
@@ -0,0 +1,15 @@
+> var x;
+undefined
+>   console.log('the value of my variable x will be: undefind')
+the value of my variable x will be: undefind
+undefined
+> console.log(x);
+undefined
+undefined
+> var x = 5;
+undefined
+>   console.log('the value of my variable x will be: 5')
+the value of my variable x will be: 5
+undefined
+> console.log(x);
+5
