2a9f5cdf9ba05b1d9e5c0447ecd7faa5bb22e254
WaelAlnadeem <35236733+WaelAlnadeem@users.noreply.github.com>
2018-02-13 17:05:48 +0100

    Update step 8.js

--- Week 2/step 8.js
+++ Week 2/step 8.js
@@ -1,1 +1,39 @@
+var x = "hello world";
+undefined
+var z = 14;
+undefined
+var w = 0;
+w = true; 
+undefined
+var cars = ["Saab", "Volvo", "BMW"];
+undefined
+
+console.log('The value of my variable foo is: ' + "hello world");
+The value of my variable foo is: hello world
+undefined
+
+console.log('The value of my variable foo is: ' + x);
+The value of my variable foo is: hello world
+undefined
+
+console.log('The value of my variable foo is: ' + 14);
+The value of my variable foo is: 14
+undefined
+
+console.log('The value of my variable foo is: ' + z);
+The value of my variable foo is: 14
+
+
+console.log('The value of my variable foo is: ' + true);
+The value of my variable foo is: true
+console.log (w);
+true
+
+console.log('The value of my variable foo is: ' + "Saab","Volvo","BMW" );
+VM374:1 The value of my variable foo is: Saab Volvo BMW
+undefined
+console.log('The value of my variable foo is: ' + cars);
+VM376:1 The value of my variable foo is: Saab,Volvo,BMW
+
+
 
