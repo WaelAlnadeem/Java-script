72368c86c68831a842fbcaf06633096b11b9d4d5
WaelAlnadeem <35236733+WaelAlnadeem@users.noreply.github.com>
2018-02-14 20:15:44 +0100

    Update step 9.js

--- Week 2/step 9.js
+++ Week 2/step 9.js
@@ -1,1 +1,18 @@
+let x = 7;
+undefined
+console.log ('the new value of x is :' + 1);
+the new value of x is :1
+undefined
+console.log(x % 3);
+1
+undefined
+let f = 34;
+undefined
+console.log(f % 4.5)
+2.5
+undefined
+console.log(f % 4)
+2
+
+
 
