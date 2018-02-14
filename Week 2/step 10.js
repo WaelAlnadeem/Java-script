01ac2f81a498e57a703d383be057d3bab5cc257d
WaelAlnadeem <35236733+WaelAlnadeem@users.noreply.github.com>
2018-02-14 20:34:19 +0100

    Create step 10.js

---
+++ Week 2/step 10.js
@@ -0,0 +1,25 @@
+let multi = [342 , 'people', 485, 'freedom'];
+undefined
+console.log(multi);
+(4) [342, "people", 485, "freedom"]
+console.log ('it could be compared becuase infinty is still a num');
+it could be compared becuase infinty is still a num
+if(o === q)
+{ 
+    console.log("they are compared");
+} else{
+    console.log("they are not compared");
+}
+they are compared
+undefined
+
+typeof o
+"number"
+typeof q
+"number"
+
+console.log (o);
+Infinity
+console.log (q);
+Infinity
+
