ae50f1ff2a91c96fe6edc61f75455976fb8718f8
WaelAlnadeem <wael.alnadeem88@gmail.com>
2018-02-17 18:14:19 +0100

    delete undefine words

--- Week 2/var x ; undefined.js
+++ Week 2/var x ; undefined.js
@@ -1,15 +1,10 @@
 > var x;
-undefined
 >   console.log('the value of my variable x will be: undefind')
-the value of my variable x will be: undefind
-undefined
+// the value of my variable x will be: undefind
 > console.log(x);
-undefined
-undefined
+// undefined
 > var x = 5;
-undefined
->   console.log('the value of my variable x will be: 5')
-the value of my variable x will be: 5
-undefined
+> console.log('the value of my variable x will be: 5')
+// the value of my variable x will be: 5
 > console.log(x);
-5
+// 5
