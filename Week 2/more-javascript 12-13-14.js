9b2014a8b75dc54923bc745c5cd0f1e3e552ee71
WaelAlnadeem <35236733+WaelAlnadeem@users.noreply.github.com>
2018-02-22 00:09:57 +0100

    Create more-javascript 12-13-14.js

---
+++ Week 2/more-javascript 12-13-14.js
@@ -0,0 +1,10 @@
+let objectA = {
+
+    teachers: ["Mauro", "Felipe", "Unmeash" ],
+    languages: ["JS", "Html and CSS", "CommandLine" ]
+   
+}
+
+console.log(`The teacher is: ${objectA.teachers[0]} for ${objectA.languages[0]} `);
+console.log(`The teacher is: ${objectA.teachers[1]} for ${objectA.languages[1]} `);
+console.log(`The teacher is: ${objectA.teachers[2]} for ${objectA.languages[2]} `);
